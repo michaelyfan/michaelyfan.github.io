233d6828d1fc70973dc25fc4bcbb4602a95b74b3
Michael Fan <michaelyfan100@gmail.com>
2017-09-17 17:55:12 -0400

    Revise design

--- js/index.js
+++ js/index.js
@@ -1,35 +1,60 @@
-$(document).ready(function() {
-    $('#background-fade').fadeOut(1000);
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // window.location.hash = hash;
+      });
+    }
+  });
 });
 
-var TitleChanger = {
-	titles: [
-    'michael <span style="color:purple;" >fan</span>',
-    '<span style="color:purple;" >student</span>',
-    '<span style="color:purple;" >designer</span>',
-    '(noob) <span style="color:purple;"> developer',
-    'late-night <span style="color:purple;" >gamer</span>',
-    '<span style="color:purple;" >master</span> home chef'
-	],
-  count: 1,
-  changeTitle: function() {
-    var title = "<p id='title-text'>" + this.titles[this.count] + "</p>";
-    this.count++;
-    if (this.count >= this.titles.length) {
-      this.count = 0;
-    }
+var yourNavigation = $(".nav");
+var stickyDiv = "nav-sticky";
+var yourHeader = $('#header-section').height();
 
-    $('#title-text').fadeOut(300, function() {
-      var t = $(title).hide();
-      $(this).replaceWith(t);
-      $('#title-text').fadeIn(300);
-    });
-
+$(window).scroll(function() {
+  if( $(this).scrollTop() > yourHeader ) {
+    yourNavigation.addClass(stickyDiv);
+  } else {
+    yourNavigation.removeClass(stickyDiv);
   }
-};
+});
 
-var handlers = {
-	changeTitle: function() {
-		TitleChanger.changeTitle();
-	}
-};
+// var TitleChanger = {
+// 	titles: [
+//     'michael <span style="color:purple;" >fan</span>',
+//     '<span style="color:purple;" >student</span>',
+//     '<span style="color:purple;" >designer</span>',
+//     '(noob) <span style="color:purple;"> developer',
+//     'late-night <span style="color:purple;" >gamer</span>',
+//     '<span style="color:purple;" >master</span> home chef'
+// 	],
+//   count: 1,
+//   changeTitle: function() {
+//     var title = "<p id='title-text'>" + this.titles[this.count] + "</p>";
+//     this.count++;
+//     if (this.count >= this.titles.length) {
+//       this.count = 0;
+//     }
+
+//     $('#title-text').fadeOut(300, function() {
+//       var t = $(title).hide();
+//       $(this).replaceWith(t);
+//       $('#title-text').fadeIn(300);
+//     });
+
+//   }
+// };
+
+// var handlers = {
+// 	changeTitle: function() {
+// 		TitleChanger.changeTitle();
+// 	}
+// };
