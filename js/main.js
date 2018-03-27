bb97435eb085b4b7e611d3e419f7d81c5c8c6383
Popguy7843 <33969138+Popguy7843@users.noreply.github.com>
2018-03-27 00:28:41 -0400

    Optimization/Compression

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
+// Smooth scroll
+$(document).ready(function () {
+
   $('.black-overlay').fadeOut(1000);
 
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
+  // Add smooth scrolling to all links
+  $('a').on('click', function (event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
       event.preventDefault();
+
+      // Store hash
       var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-
+      }, 800, function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
         // window.location.hash = hash;
       });
     }
@@ -21,8 +30,8 @@ var yourNavigation = $(".nav");
 var stickyDiv = "nav-sticky";
 var yourHeader = $('#header-section').height();
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > yourHeader ) {
+$(window).scroll(function () {
+  if ($(this).scrollTop() > yourHeader) {
     yourNavigation.addClass(stickyDiv);
   } else {
     yourNavigation.removeClass(stickyDiv);
